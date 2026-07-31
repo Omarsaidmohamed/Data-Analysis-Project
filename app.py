465b41ea7a0ef67f0caf2d2c68bb0d3531c2aca0
Omarsaidmohamed <os720266@gmail.com>
2026-08-01 01:18:38 +0300

    Enhance dashboard
    Added functionality to define remote jobs and display KPIs. Included tabs for job analysis and skills visualization.

--- app.py
+++ app.py
@@ -1,44 +1,43 @@
+# import libraries
 import streamlit as st
 import pandas as pd
 import plotly.express as px
 import seaborn as sns
 import matplotlib.pyplot as plt
-# st.file_uploader("Upload file", type={"csv"})
+
 st.title("Data Analyst Job Market Dashboard")
-# if cleaned_job_postings.csv is not None:
 Job_Postings = pd.read_csv("cleaned_job_postings.csv") 
 
-
+# Define remote job
 remote_jobs=[]
 for remote in Job_Postings["work_type"]:
     if remote == "Remote":
         remote_jobs.append(remote)
+
+#     KPIs
 col1, col2, col3, col4 = st.columns(4)
 col1.metric("Total Jobs", len(Job_Postings))
 col2.metric("Companies", Job_Postings["company_name"].nunique())
 col3.metric("Average Salary",f"${Job_Postings["salary_avg"].mean():,.0f}")
 col4.metric("Remote Jobs", len(remote_jobs))
 
+# -------- SLIDER -----
 n_rows= st.slider("Choose number of rows to display" , min_value=5 ,max_value=len(Job_Postings))
 colmns_to_show = st.multiselect("Select colmns to show" , Job_Postings.columns.tolist(), default=Job_Postings.columns.tolist())
 st.write(Job_Postings[:n_rows][colmns_to_show])
 
-
-
-    
-    # work_type = st.sidebar.selectbox("Select Work Type",["All"] + sorted(Job_Postings["work_type"].dropna().unique().tolist()))
-
-
+#     Taps
 tab1 , tab2 , tab3 = st.tabs(["Job Analysis" , "Salary" , "Skills"])
 
 with tab1:
+# ----- Top 10 Job Titles -----
     top_titles = Job_Postings["title"].value_counts().head(10).sort_values()
     fig = px.bar(x=top_titles.values , y=top_titles.index , title="Top 10 Job Titles" , labels={"x" : "Number Of Top Jobs Postings" , "y" : "Job Title" ,})
     fig.update_traces(text=top_titles.values,textposition="outside")
-    # st.pyplot(fig)
     st.plotly_chart(fig)
     st.divider()
 
+#  ------ Most Freq Locations In Job Postings -------
     most_locations = Job_Postings["location"].value_counts().head(10)
     fig,ax = plt.subplots(figsize=(10,4))
     sns.barplot(x= most_locations.values , y=most_locations.index, color="#FF5C85")
@@ -94,7 +93,7 @@ with tab3:
     freq_skills_series = pd.Series(freq_skills_list)
     tob_skills = freq_skills_series.value_counts().head(10)
 
-
+#   ------ Top  Frequanced Skills -------
     fig, ax = plt.subplots(figsize=(10,4))
     plt.barh(tob_skills.index, tob_skills.values , color="Orange")
     ax.set_title("Top 10 Frequanced Skills" , weight="bold" )
@@ -106,7 +105,7 @@ with tab3:
     st.pyplot(fig)
     st.divider()
 
-
+#  ------- Skills VS Salary --------
     fig,ax = plt.subplots(figsize=(10,4))
     sns.regplot(
         data=Job_Postings,
